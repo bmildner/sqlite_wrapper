155928fa131490cefdd8e8e0071ec90ef2dcda98
Bertolt Mildner <bmildner@gmx.at>
2024-03-25 07:32:50 +0100

    added more function to SQLite3 mock

--- test/mocks/sqlite_mock.cpp
+++ test/mocks/sqlite_mock.cpp
@@ -18,3 +18,33 @@ int sqlite3_prepare_v2(sqlite3* db, const char* zSql, int nByte, sqlite3_stmt** 
 {
   return get_global_mock<sqlite3_mock>()->sqlite3_prepare_v2(db, zSql, nByte, ppStmt, pzTail);
 }
+
+int sqlite3_finalize(sqlite3_stmt* pStmt)
+{
+  return get_global_mock<sqlite3_mock>()->sqlite3_finalize(pStmt);
+}
+
+int sqlite3_bind_null(sqlite3_stmt* pStmt, int index)
+{
+  return get_global_mock<sqlite3_mock>()->sqlite3_bind_null(pStmt, index);
+}
+
+int sqlite3_bind_int64(sqlite3_stmt* pStmt, int index, sqlite3_int64 value)
+{
+  return get_global_mock<sqlite3_mock>()->sqlite3_bind_int64(pStmt, index, value);
+}
+
+int sqlite3_bind_text64(sqlite3_stmt* pStmt, int index, const char* value, sqlite3_uint64 byteSize, void(*pDtor)(void*), unsigned char encoding)
+{
+  return get_global_mock<sqlite3_mock>()->sqlite3_bind_text64(pStmt, index, value, byteSize, pDtor, encoding);
+}
+
+int sqlite3_bind_blob64(sqlite3_stmt* pStmt, int index, const void* value, sqlite3_uint64 byteSize, void(*pDtor)(void*))
+{
+  return get_global_mock<sqlite3_mock>()->sqlite3_bind_blob64(pStmt, index, value, byteSize, pDtor);
+}
+
+int sqlite3_bind_double(sqlite3_stmt* pStmt, int index, double value)
+{
+  return get_global_mock<sqlite3_mock>()->sqlite3_bind_double(pStmt, index, value);
+}
--- test/mocks/sqlite_mock.h
+++ test/mocks/sqlite_mock.h
@@ -16,6 +16,14 @@ namespace sqlite_wrapper::mocks
     MOCK_METHOD(int, sqlite3_close, (sqlite3* pDb), (const));
 
     MOCK_METHOD(int, sqlite3_prepare_v2, (sqlite3* db, const char* zSql, int nByte, sqlite3_stmt** ppStmt, const char** pzTail), (const));
+    MOCK_METHOD(int, sqlite3_finalize, (sqlite3_stmt* pStmt), (const));
+
+    MOCK_METHOD(int, sqlite3_bind_null, (sqlite3_stmt* pStmt, int index), (const));
+    MOCK_METHOD(int, sqlite3_bind_int64, (sqlite3_stmt* pStmt, int index, sqlite3_int64 value), (const));
+    MOCK_METHOD(int, sqlite3_bind_text64, (sqlite3_stmt* pStmt, int index, const char* value, sqlite3_uint64 byteSize,void(*pDtor)(void*), unsigned char encoding), (const));
+    MOCK_METHOD(int, sqlite3_bind_blob64, (sqlite3_stmt* pStmt, int index, const void* value, sqlite3_uint64 byteSize, void(*pDtor)(void*)), (const));
+    MOCK_METHOD(int, sqlite3_bind_double, (sqlite3_stmt* pStmt, int index, double value), (const));
+
   };
 
 }  // namespace sqlite_wrapper::mocks
