466e78ee3e9392d06d04004b38733804fea4394c
Bertolt Mildner <bmildner@gmx.at>
2024-03-29 21:56:12 +0100

    extracted running test_runner and test_runner_mocked from target coverage_report into new target run_all_tests

--- coverage.cmake
+++ coverage.cmake
@@ -1,3 +1,11 @@
+add_custom_target(run_all_tests
+        COMMAND test_runner
+        COMMAND test_runner_mocked
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        USES_TERMINAL)
+
+add_dependencies(run_all_tests test_runner test_runner_mocked)
+
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
   find_program(GCOVR NAMES gcovr)
 
@@ -5,13 +13,11 @@ if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     message(WARNING "No coverage analysis, gcovr not found!")
   else()
     add_custom_target(coverage_report
-            COMMAND test_runner
-            COMMAND test_runner_mocked
             COMMAND ${GCOVR} -r ${PROJECT_SOURCE_DIR} -e ../vcpkg_installed/ -e ${PROJECT_SOURCE_DIR}/test --sort-percentage --txt-metric branch -exclude-unreachable-branches --exclude-throw-branches
             COMMAND ${GCOVR} -r ${PROJECT_SOURCE_DIR} -e ../vcpkg_installed/ -e ${PROJECT_SOURCE_DIR}/test --sort-percentage --txt-metric line -exclude-function-lines --exclude-noncode-lines
             WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
             USES_TERMINAL)
 
-    add_dependencies(coverage_report test_runner test_runner_mocked)
+    add_dependencies(coverage_report run_all_tests)
     endif()
 endif()
