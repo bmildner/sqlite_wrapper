f28457b6dd4b924b48c2f2b0c2219f899f89411d
Bertolt Mildner <bmildner@gmx.at>
2024-04-04 06:20:17 +0200

    moved static analysis into new file static_analysis.cmake and added cache option for fast and easy switching

---
+++ static_analysis.cmake
@@ -0,0 +1,16 @@
+# turn off locally for faster builds, do NOT commit when disabled!
+set(ENABLE_STATIC_ANALYSIS ON CACHE BOOL "Enables static analysis with tools like clang-tidy" FORCE)
+
+if (ENABLE_STATIC_ANALYSIS)
+  # search for clang-tidy
+  if (DEFINED ENV{CLANG_TIDY})
+    set(CLANG_TIDY $ENV{CLANG_TIDY})
+  else()
+    find_program(CLANG_TIDY NAMES clang-tidy clang-tidy-* REQUIRED)
+  endif()
+
+  # enable clang-tidy
+  if (NOT DEFINED MSVC)
+    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY}; -extra-arg=-Wno-unknown-warning-option -extra-arg=-Wno-ignored-optimization-argument)
+  endif()
+endif()
