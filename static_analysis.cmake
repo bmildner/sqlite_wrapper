3c130f9471633b0cbefbbf6fd5f2db8cae4c753d
Bertolt Mildner <bmildner@gmx.at>
2024-04-04 20:27:00 +0200

    fixed "not enabling" of static analysis with MSVC

--- static_analysis.cmake
+++ static_analysis.cmake
@@ -1,7 +1,7 @@
 # turn off locally for faster builds, do NOT commit when disabled!
 set(ENABLE_STATIC_ANALYSIS ON CACHE BOOL "Enables static analysis with tools like clang-tidy" FORCE)
 
-if (ENABLE_STATIC_ANALYSIS)
+if ((ENABLE_STATIC_ANALYSIS) AND (NOT DEFINED MSVC))
   # search for clang-tidy
   if (DEFINED ENV{CLANG_TIDY})
     set(CLANG_TIDY $ENV{CLANG_TIDY})
@@ -10,7 +10,5 @@ if (ENABLE_STATIC_ANALYSIS)
   endif()
 
   # enable clang-tidy
-  if (NOT DEFINED MSVC)
-    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY}; -extra-arg=-Wno-unknown-warning-option -extra-arg=-Wno-ignored-optimization-argument)
-  endif()
+  set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY}; -extra-arg=-Wno-unknown-warning-option -extra-arg=-Wno-ignored-optimization-argument)
 endif()
