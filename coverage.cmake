8da39e135e5c0b98449f882e997085b024729a6a
Bertolt Mildner <bmildner@_gmx.at>
2024-11-03 12:51:40 +0100

    fixed GCOVR parameters

--- coverage.cmake
+++ coverage.cmake
@@ -13,8 +13,8 @@ if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     message(WARNING "No coverage analysis, gcovr not found!")
   else()
     add_custom_target(coverage_report
-            COMMAND ${GCOVR} -r ${CMAKE_CURRENT_SOURCE_DIR} -e ../vcpkg_installed/ -e ${CMAKE_CURRENT_SOURCE_DIR}/test --sort-percentage --txt-metric branch -exclude-unreachable-branches --exclude-throw-branches
-            COMMAND ${GCOVR} -r ${CMAKE_CURRENT_SOURCE_DIR} -e ../vcpkg_installed/ -e ${CMAKE_CURRENT_SOURCE_DIR}/test --sort-percentage --txt-metric line -exclude-function-lines --exclude-noncode-lines
+            COMMAND ${GCOVR} -r ${CMAKE_CURRENT_SOURCE_DIR} -e ../vcpkg_installed/ -e ${CMAKE_CURRENT_SOURCE_DIR}/test --sort uncovered-percent --txt-metric branch -exclude-unreachable-branches --exclude-throw-branches
+            COMMAND ${GCOVR} -r ${CMAKE_CURRENT_SOURCE_DIR} -e ../vcpkg_installed/ -e ${CMAKE_CURRENT_SOURCE_DIR}/test --sort uncovered-percent --txt-metric line -exclude-function-lines --exclude-noncode-lines
             WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
             USES_TERMINAL)
 
