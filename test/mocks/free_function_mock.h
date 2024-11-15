b074dcb93980c5ce0aae37005052ad0390b435cd
Bertolt Mildner <bmildner@gmx.at>
2024-11-15 13:32:03 +0100

    function now sqlite_wrapper::mocks::get_global_mock() adds caller to exception msg, renamed concept mock to MockClass

--- test/mocks/free_function_mock.h
+++ test/mocks/free_function_mock.h
@@ -2,25 +2,28 @@
 
 #include <gmock/gmock.h>
 
-#include <concepts>
+#include <source_location>
 #include <type_traits>
 #include <memory>
 #include <stdexcept>
 
+// format.h is header only, but still this is a bit of a hack ...
+#include "sqlite_wrapper/format.h"
+
 namespace sqlite_wrapper::mocks
 {
   template <typename T>
-  concept mock = std::is_class_v<T> && std::has_virtual_destructor_v<T>;
+  concept MockClass = std::is_class_v<T> && std::has_virtual_destructor_v<T>;
 
-  template <mock Mock>
+  template <MockClass Mock>
   using strict_mock = testing::StrictMock<Mock>;
 
-  template <mock Mock>
+  template <MockClass Mock>
   using mock_ptr = std::shared_ptr<strict_mock<Mock>>;
 
   namespace details
   {
-    template <mock Mock>
+    template <MockClass Mock>
     auto get_set_reset_global_mock(const mock_ptr<Mock>& ptr, bool reset) -> const mock_ptr<Mock>&
     {
       static mock_ptr<Mock> g_mock{};
@@ -38,13 +41,13 @@ namespace sqlite_wrapper::mocks
     }
   }  // namespace details
 
-  template <mock Mock>
+  template <MockClass Mock>
   void reset_global_mock()
   {
     details::get_set_reset_global_mock<Mock>(nullptr, true);
   }
 
-  template <mock Mock>
+  template <MockClass Mock>
   void set_global_mock(const mock_ptr<Mock>& ptr)
   {
     if (ptr == nullptr)
@@ -57,7 +60,7 @@ namespace sqlite_wrapper::mocks
     }
   }
 
-  template <mock Mock, typename... Args>
+  template <MockClass Mock, typename... Args>
   auto create_and_set_global_mock(Args&&... args) -> mock_ptr<Mock>
   {
     auto ptr{std::make_shared<strict_mock<Mock>>(std::forward<Args>(args)...)};
@@ -67,8 +70,8 @@ namespace sqlite_wrapper::mocks
     return ptr;
   }
 
-  template <mock Mock>
-  auto get_global_mock() -> const mock_ptr<Mock>&
+  template <MockClass Mock>
+  auto get_global_mock(const std::source_location& location = std::source_location::current()) -> const mock_ptr<Mock>&
   {
     // false-positive for dangling reference from GCC 13, clang-tidy 16 does not know the warning option
 #if defined(__GNUC__) && (__GNUC__ >= 13) && !defined(__clang__)
@@ -82,8 +85,8 @@ namespace sqlite_wrapper::mocks
 
     if (ptr == nullptr)
     {
-      // TODO: maybe add caller source_location ...
-      throw std::runtime_error(std::string{"failed to get global mock object for type "} + typeid(Mock).name());
+      throw std::runtime_error(sqlite_wrapper::format("failed to get global mock object for type {} at {}",
+        typeid(Mock).name(), location));
     }
 
     return ptr;
