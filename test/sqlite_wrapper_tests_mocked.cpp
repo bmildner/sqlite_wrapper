9d1aeb4219ecd39a37a7489ad27d16c1f22b1b88
Bertolt Mildner <bmildner@_gmx.at>
2024-11-16 15:28:37 +0100

    formatted code in sqlite_wrapper_tests_mocked.cpp

--- test/sqlite_wrapper_tests_mocked.cpp
+++ test/sqlite_wrapper_tests_mocked.cpp
@@ -1,39 +1,38 @@
-#include <string_view>
-#include <string>
-#include <vector>
-#include <list>
 #include <array>
+#include <list>
 #include <memory>
-#include <set>
 #include <ranges>
+#include <set>
+#include <string>
+#include <string_view>
+#include <vector>
 
 #include <gtest/gtest.h>
 
+#include "assert_throw_msg.h"
+#include "free_function_mock.h"
+#include "sqlite_mock.h"
 #include "sqlite_wrapper/format.h"
 #include "sqlite_wrapper/sqlite_wrapper.h"
 
-#include "sqlite_mock.h"
-#include "free_function_mock.h"
-#include "assert_throw_msg.h"
-
-using sqlite_wrapper::mocks::sqlite3_mock;
 using sqlite_wrapper::mocks::reset_global_mock;
+using sqlite_wrapper::mocks::sqlite3_mock;
 
-using testing::Mock;
-using testing::Test;
-using testing::StrEq;
-using testing::NotNull;
+using testing::AllOf;
+using testing::DoAll;
+using testing::ElementsAreArray;
+using testing::Eq;
+using testing::HasSubstr;
+using testing::InSequence;
+using testing::Invoke;
 using testing::IsNull;
+using testing::Mock;
+using testing::NotNull;
 using testing::Return;
 using testing::SetArgPointee;
-using testing::DoAll;
-using testing::Eq;
 using testing::StartsWith;
-using testing::HasSubstr;
-using testing::AllOf;
-using testing::Invoke;
-using testing::ElementsAreArray;
-using testing::InSequence;
+using testing::StrEq;
+using testing::Test;
 
 extern "C"
 {
@@ -58,7 +57,7 @@ namespace
 
   class sqlite_wrapper_mocked_tests : public Test
   {
-  public:
+   public:
     sqlite_wrapper_mocked_tests() = default;
     ~sqlite_wrapper_mocked_tests() override = default;
 
@@ -83,50 +82,47 @@ namespace
 
     static auto expect_null_bind() -> expect_bind_function
     {
-      return [] (::sqlite3_stmt* stmt, int index)
-      {
-        EXPECT_CALL(*get_mock(), sqlite3_bind_null(stmt, index)).WillOnce(Return(SQLITE_OK)).RetiresOnSaturation();
-      };
+      return [](::sqlite3_stmt* stmt, int index)
+      { EXPECT_CALL(*get_mock(), sqlite3_bind_null(stmt, index)).WillOnce(Return(SQLITE_OK)).RetiresOnSaturation(); };
     }
 
     static auto expect_int64_bind(std::int64_t value) -> expect_bind_function
     {
-      return [value] (::sqlite3_stmt* stmt, int index)
-      {
-        EXPECT_CALL(*get_mock(), sqlite3_bind_int64(stmt, index, value)).WillOnce(Return(SQLITE_OK)).RetiresOnSaturation();
-      };
+      return [value](::sqlite3_stmt* stmt, int index)
+      { EXPECT_CALL(*get_mock(), sqlite3_bind_int64(stmt, index, value)).WillOnce(Return(SQLITE_OK)).RetiresOnSaturation(); };
     }
 
     static auto expect_double_bind(double value) -> expect_bind_function
     {
-      return [value] (::sqlite3_stmt* stmt, int index)
-      {
-        EXPECT_CALL(*get_mock(), sqlite3_bind_double(stmt, index, value)).WillOnce(Return(SQLITE_OK)).RetiresOnSaturation();
-      };
+      return [value](::sqlite3_stmt* stmt, int index)
+      { EXPECT_CALL(*get_mock(), sqlite3_bind_double(stmt, index, value)).WillOnce(Return(SQLITE_OK)).RetiresOnSaturation(); };
     }
 
     static auto expect_text_bind(const std::string& value) -> expect_bind_function
     {
-      return [value] (::sqlite3_stmt* stmt, int index)
+      return [value](::sqlite3_stmt* stmt, int index)
       {
         EXPECT_CALL(*get_mock(), sqlite3_bind_text64(stmt, index, StrEq(value), value.size(), IsNull(), SQLITE_UTF8))
-            .WillOnce(Return(SQLITE_OK)).RetiresOnSaturation();
+            .WillOnce(Return(SQLITE_OK))
+            .RetiresOnSaturation();
       };
     }
 
     static auto expect_blob_bind(const sqlite_wrapper::byte_vector& value) -> expect_bind_function
     {
-      return [value] (::sqlite3_stmt* stmt, int index)
+      return [value](::sqlite3_stmt* stmt, int index)
       {
         EXPECT_CALL(*get_mock(), sqlite3_bind_blob64(stmt, index, NotNull(), value.size(), IsNull()))
-            .WillOnce(DoAll(Invoke([value] (sqlite3_stmt*, int, const void* param_value, sqlite3_uint64 byteSize, void(*)(void*))
-                                   {
+            .WillOnce(DoAll(Invoke(
+                                [value](sqlite3_stmt*, int, const void* param_value, sqlite3_uint64 byteSize, void (*)(void*))
+                                {
                                   const auto data{sqlite_wrapper::const_byte_span{static_cast<const std::byte*>(param_value),
                                                                                   static_cast<std::size_t>(byteSize)}};
 
-                                     ASSERT_THAT(data, ElementsAreArray(value));
-                                   }), Return(SQLITE_OK)))
-        .RetiresOnSaturation();
+                                  ASSERT_THAT(data, ElementsAreArray(value));
+                                }),
+                            Return(SQLITE_OK)))
+            .RetiresOnSaturation();
       };
     }
 
@@ -174,9 +170,7 @@ namespace
     EXPECT_CALL(*get_mock(), sqlite3_open_v2(StrEq(file_name), NotNull(), flags, nullptr))
         .WillOnce(DoAll(SetArgPointee<1>(&database), Return(SQLITE_OK)))
         .RetiresOnSaturation();
-    EXPECT_CALL(*get_mock(), sqlite3_close(Eq(&database)))
-        .WillOnce(Return(SQLITE_OK))
-        .RetiresOnSaturation();
+    EXPECT_CALL(*get_mock(), sqlite3_close(Eq(&database))).WillOnce(Return(SQLITE_OK)).RetiresOnSaturation();
   }
 
   auto sqlite_wrapper_mocked_tests::expect_and_get_database() -> sqlite_wrapper::database
@@ -196,7 +190,8 @@ namespace
     // make sure mocks are called in the correct order!
     const InSequence sequence_guard;
 
-    EXPECT_CALL(*get_mock(), sqlite3_prepare_v2(database.value, StrEq(dummy_sql), static_cast<int>(dummy_sql.size()), NotNull(), IsNull()))
+    EXPECT_CALL(*get_mock(),
+                sqlite3_prepare_v2(database.value, StrEq(dummy_sql), static_cast<int>(dummy_sql.size()), NotNull(), IsNull()))
         .WillOnce(DoAll(SetArgPointee<3>(&statement), Return(SQLITE_OK)));
 
     int index{1};
@@ -240,16 +235,18 @@ TEST_F(sqlite_wrapper_mocked_tests, open_success)
 TEST_F(sqlite_wrapper_mocked_tests, open_fails)
 {
   EXPECT_CALL(*get_mock(), sqlite3_open_v2(StrEq(db_file_name), NotNull(), SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nullptr))
-    .WillOnce(Return(SQLITE_INTERNAL))
-    .WillOnce(DoAll(SetArgPointee<1>(nullptr), Return(SQLITE_OK)));
+      .WillOnce(Return(SQLITE_INTERNAL))
+      .WillOnce(DoAll(SetArgPointee<1>(nullptr), Return(SQLITE_OK)));
   EXPECT_CALL(*get_mock(), sqlite3_errstr(SQLITE_INTERNAL)).WillOnce(Return("SQLITE_INTERNAL"));
   EXPECT_CALL(*get_mock(), sqlite3_errstr(SQLITE_ERROR)).Times(2).WillRepeatedly(Return("SQLITE_ERROR"));
 
-  ASSERT_THROW_MSG((void) sqlite_wrapper::open(db_file_name), sqlite_wrapper::sqlite_error,
-    AllOf(StartsWith(sqlite_wrapper::format("sqlite3_open() failed to open database \"{}\"", db_file_name)), HasSubstr("SQLITE_INTERNAL")));
+  ASSERT_THROW_MSG((void)sqlite_wrapper::open(db_file_name), sqlite_wrapper::sqlite_error,
+                   AllOf(StartsWith(sqlite_wrapper::format("sqlite3_open() failed to open database \"{}\"", db_file_name)),
+                         HasSubstr("SQLITE_INTERNAL")));
 
   ASSERT_THROW_MSG((void)sqlite_wrapper::open(db_file_name), sqlite_wrapper::sqlite_error,
-    AllOf(StartsWith(sqlite_wrapper::format("sqlite3_open() returned nullptr for database \"{}\"", db_file_name)), HasSubstr("SQLITE_ERROR")));
+                   AllOf(StartsWith(sqlite_wrapper::format("sqlite3_open() returned nullptr for database \"{}\"", db_file_name)),
+                         HasSubstr("SQLITE_ERROR")));
 
   // NOLINTNEXTLINE(hicpp-signed-bitwise)
   const sqlite_wrapper::open_flags bad_open_flags{to_underlying(sqlite_wrapper::open_flags::open_only) |
