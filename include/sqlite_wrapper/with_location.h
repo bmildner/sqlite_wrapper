3e2ccb6154dc9e838f6d827c0d9302b1ed7f71f2
Bertolt Mildner <bmildner@_gmx.at>
2025-02-02 13:00:25 +0100

    added some doxygen docu to with_location class

--- include/sqlite_wrapper/with_location.h
+++ include/sqlite_wrapper/with_location.h
@@ -5,10 +5,19 @@
 
 namespace sqlite_wrapper
 {
+  /**
+   * Wrapper that adds a std::source_location to any type.
+   * Useful in cases where one can not have a defaulted std::source_location as the last parameter due to perfect forwarding.
+   *
+   * @tparam T the type to be wrapped
+   */
   template <typename T>
   struct with_location
   {
-    using value_type = std::remove_reference_t<T>;
+    /**
+     * = std::decay_t<T>
+     */
+    using value_type = std::decay_t<T>;
 
     // NOLINTNEXTLINE(hicpp-explicit-conversions)
     with_location(const value_type& value, const std::source_location& loc = std::source_location::current())
@@ -19,6 +28,15 @@ namespace sqlite_wrapper
     with_location(value_type&& value, const std::source_location& loc = std::source_location::current())
       : value(std::move(value)), location(loc)
     {}
+
+    with_location() = delete;
+    ~with_location() = default;
+
+    with_location(const with_location&) = default;
+    with_location(with_location&&) = default;
+
+    auto operator=(const with_location&) -> with_location& = default;
+    auto operator=(with_location&&) -> with_location& = default;
 
     value_type value;
     std::source_location location;
