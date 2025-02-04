96acb23fbf063cc226e9211a7ffafa56e4c936ee
Bertolt Mildner <bmildner@gmx.at>
2025-02-04 19:17:58 +0100

    added doxygen docu in raii.h

--- include/sqlite_wrapper/raii.h
+++ include/sqlite_wrapper/raii.h
@@ -16,20 +16,38 @@ namespace sqlite_wrapper
 {
   namespace details
   {
+    /**
+     * Custom deleter for database handles RAII-guard using std::unique_ptr.
+     */
     struct database_deleter
     {
       SQLITE_WRAPPER_EXPORT void operator()(::sqlite3* raw_db_handle) const noexcept;
     };
 
+    /**
+     * Custom deleter for prepared statement handles RAII-guard using std::unique_ptr.
+     */
     struct statement_deleter
     {
       SQLITE_WRAPPER_EXPORT void operator()(::sqlite3_stmt* stmt) const noexcept;
     };
   }  // namespace details
 
+  /**
+   * RAII-guard for database handles.
+   */
   using database = std::unique_ptr<::sqlite3, details::database_deleter>;
+  /**
+   * RAII-guard for prepared statement handles.
+   */
   using statement = std::unique_ptr<::sqlite3_stmt, details::statement_deleter>;
 
+  /**
+   * *Non-owning* database handle with an std::source_location .
+   */
   using db_with_location = with_location<sqlite3*>;
+  /**
+   * *Non-owning* prepared statement handle with an std::source_location .
+   */
   using stmt_with_location = with_location<sqlite3_stmt*>;
 }  // namespace sqlite_wrapper
