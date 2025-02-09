5b8593bb93b862d0cf4945e02ff4c8e7bca1a965
Bertolt Mildner <bmildner@gmx.at>
2025-02-09 23:33:16 +0100

    added some comments on library configuration

--- include/sqlite_wrapper/config.h
+++ include/sqlite_wrapper/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#ifdef SQLITE_WRAPPER_SHARED
-#  ifdef sqlite_wrapper_EXPORTS
+#ifdef SQLITE_WRAPPER_SHARED  // set as PUBLIC for sqlite_wrapper target
+#  ifdef sqlite_wrapper_EXPORTS  // automatically set by cmake when building sqlite_wrapper target
 #    ifdef _WIN32
 #       define SQLITE_WRAPPER_EXPORT __declspec(dllexport)
 #    else
@@ -14,6 +14,6 @@
 #      define SQLITE_WRAPPER_EXPORT
 #    endif
 #  endif
-#else
+#else  // ignore SQLITE_WRAPPER_EXPORT when building or using sqlite_wrapper_static
 #  define SQLITE_WRAPPER_EXPORT
 #endif
