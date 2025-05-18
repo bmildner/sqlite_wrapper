c73326558fefba39394a491a26f609152b1810f8
Bertolt Mildner <bmildner@gmx.at>
2025-05-18 16:25:59 +0200

    avoid unreachable code warning with msvc

--- test/sqlite_wrapper_tests.cpp
+++ test/sqlite_wrapper_tests.cpp
@@ -81,8 +81,11 @@ TEST_F(sqlite_wrapper_tests, open_failes)
 
 TEST_F(sqlite_wrapper_tests, assert_throw_with_msg)
 {
-  const std::source_location location{};
+  std::source_location location{};
 
-  ASSERT_THROW_WITH_MSG(throw std::runtime_error{"bad"}, sqlite_wrapper::sqlite_error, AllOf(StartsWith("sqlite3_open() failed to open database"), HasSubstr("failed with: unable to open database file"),
+  ASSERT_THROW_WITH_MSG(
+      location = std::source_location::current(); (void)sqlite_wrapper::open(temp_db_file_name.string(), sqlite_wrapper::open_flags::open_only), std::runtime_error,
+      AllOf(StartsWith("sqlite3_open() failed to open database"), HasSubstr("failed with: unable to open database file"),
                 HasSubstr(temp_db_file_name.string()), HasSubstr(location.file_name()), HasSubstr(location.function_name())));
+
 }
